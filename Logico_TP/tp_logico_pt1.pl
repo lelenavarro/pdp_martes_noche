a3d240072d62148d67e7ed244826d1574247baf7
Leandro Navarro <lelenavarro@hotmail.com>
2017-07-04 14:55:25 -0300

    Puntos faltantes de la parte 1 y casos de prueba
    corregi punto 1 y 2. termine punte 3. hice casos de prueba

--- Logico_TP/tp_logico_pt1.pl
+++ Logico_TP/tp_logico_pt1.pl
@@ -1,29 +1,57 @@
 /* GRUPO 01 - TP LOGICO Programadores */
-/* PARTE 01 */
+/* PARTE 1 */
 
 /* Requerimientos Basicos */
-/* Punto 01 */
-programaEn(fernando, cobol).
-programaEn(fernando, visualBasic).
-programaEn(fernando, java).
+/* Punto 1 */
+programaEn(isabel, cobol).
+programaEn(isabel, visualBasic).
+programaEn(isabel, java).
 programaEn(julieta, java).
 programaEn(marcos, java).
 /*programaEn(julieta, go) no se sabe */
 programaEn(santiago, java).
 programaEn(santiago, ecmaScript).
 
-trabajaDe(fernando, analistaFuncional).
+trabajaDe(isabel, analistaFuncional).
 trabajaDe(andres, proyectLeader).
 
 esProgramador(Persona):- programaEn(Persona, _).
 
-/* Punto 02 */
+/* Punto 2 */
 /*
-programaEn(fernando, Lenguaje).
+programaEn(isabel, Lenguaje).
 programaEn(Persona, java).
 programaEn(Persona, assembler).
-esProgramador(fernando).
-trabajaDe(fernando, Rol).
+esProgramador(isabel).
+trabajaDe(isabel, Rol).
 esProgramador(Persona).
 trabajaDe(Persona, proyectLeader).
+*/
+
+/* Punto 3: Proyectos */
+proyecto(sumatra, java).
+proyecto(sumatra, net).
+proyecto(prometeus, cobol).
+
+trabajaEn(isabel, prometeus).
+trabajaEn(santiago, prometeus).
+trabajaEn(julieta, sumatra).
+trabajaEn(marcos, sumatra).
+trabajaEn(andres, sumatra).
+
+proyectoCorrecto(Persona, Proyecto):- trabajaEn(Persona, Proyecto), proyecto(Proyecto, Lenguaje), programaEn(Persona, Lenguaje).
+proyectoCorrecto(Persona, Proyecto):- trabajaEn(Persona, Proyecto), trabajaDe(Persona, analistaFuncional).
+proyectoCorrecto(Persona, Proyecto):- trabajaEn(Persona, Proyecto), trabajaDe(Persona, proyectLeader).
+
+/* Punto 3.1: Casos de Prueba*/
+/*
+a. proyecto(sumatra, Lenguaje).
+b. proyecto(prometeus, Lenguaje), Lenguaje \= cobol.
+c. trabajaEn(isabel, prometeus).
+d. trabajaEn(santiago, prometeus).
+e. trabajaEn(Persona, sumatra).
+f. proyectoCorrecto(Persona, sumatra).
+g. proyectoCorrecto(Persona, prometeus).
+h. proyectoCorrecto(Persona, _).
+i. proyectoCorrecto(_, Proyecto).
 */
